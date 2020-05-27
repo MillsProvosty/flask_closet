e82bac39eaf0ce94348e27d1765a5fc1e9e81c04
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-05-27 12:55:30 -0600

    Changes to app file

--- app.py
+++ app.py
@@ -17,38 +17,11 @@ POSTGRES = {
 }
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
 ma = Marshmallow(app)
 
-
-# Models:
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    username = db.Column(db.String(100))
-
-    def __init__(self, email, password, username):
-        self.email = email
-        self.password = password
-        self.username = username
-
-
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(100))
-    occasion = db.Column(db.String(100))
-    color = db.Column(db.String(100))
-    season = db.Column(db.String(100))
-    image = db.Column(db.String(500))
-
-    def __init__(self, type, occasion, color, season, image):
-        self.type = type
-        self.occasion = occasion
-        self.color = color
-        self.season = season
-        self.image = image
 
 
 # Schemas:
@@ -57,17 +30,22 @@ class UserSchema(ma.Schema):
         fields = ('id', 'username', 'email', 'password')
 
 
+class ItemSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'type', 'occasion', 'color', 'season', 'image')
+
+
 # Init Schema:
 user_schema = UserSchema()
 
-#
+item_schema = ItemSchema()
+
+# Routes:
+
 # @app.route('/')
 # def hello_world():
 #     return 'Hello World!'
 
-
-# db.create_all()
-guest = User(email='guest@example.com', password='password', username='Mills')
 
 manager = Manager(app)
 migrate = Migrate(app, db)
